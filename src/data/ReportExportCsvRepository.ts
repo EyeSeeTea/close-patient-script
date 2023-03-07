03b1ca623e027a98802c2463c586ec3e0135e973
p3rcypj <pablo@percypj.com>
2023-03-07 12:18:30 +0000

    Create csv

--- src/data/ReportExportCsvRepository.ts
+++ src/data/ReportExportCsvRepository.ts
@@ -1,52 +1,30 @@
 import _ from "lodash";
-import { Async } from "domain/entities/Async";
+import * as CsvWriter from "csv-writer";
 import { ReportExportRepository, ReportExportSaveOptions } from "domain/repositories/ReportExportRepository";
+import { Async } from "domain/entities/Async";
 
 export class ReportExportCsvRepository implements ReportExportRepository {
     async save(options: ReportExportSaveOptions): Async<void> {
-        // const { events, outputPath: reportPath } = options;
-        // const createCsvWriter = CsvWriter.createObjectCsvWriter;
-        // const csvHeader = _.map(headers, (obj, key) => ({ id: key, ...obj }));
-        // const csvWriter = createCsvWriter({ path: reportPath, header: csvHeader });
-        // const formatObj = (obj: NamedRef) => `${obj.name.trim()} [${obj.id}]`;
-        // const records = events.map((event): Row => {
-        //     return {
-        //         id: event.id,
-        //         program: formatObj(event.program),
-        //         programStage: formatObj(event.programStage),
-        //         orgUnit: formatObj(event.orgUnit),
-        //         teiId: event.trackedEntityInstanceId || "-",
-        //         dataValues: event.dataValues.map(dv => `${dv.dataElementId}=${dv.value}`).join(", "),
-        //         created: event.created,
-        //         date: event.date,
-        //         status: event.status,
-        //     };
-        // });
-        // await csvWriter.writeRecords(records);
+        const { entities, outputPath: reportPath, programId } = options;
+        const createCsvWriter = CsvWriter.createObjectCsvWriter;
+        const csvHeader = _.map(headers, (obj, key) => ({ id: key, ...obj }));
+        const csvWriter = createCsvWriter({ path: reportPath, header: csvHeader });
+        const records = entities.map((entity): Row => {
+            return {
+                programId: programId,
+                trackedEntityId: entity.trackedEntity,
+                orgUnitId: entity.orgUnit,
+            };
+        });
+        await csvWriter.writeRecords(records);
     }
 }
 
-type Attr =
-    | "id"
-    | "program"
-    | "programStage"
-    | "orgUnit"
-    | "teiId"
-    | "dataValues"
-    | "created"
-    | "status"
-    | "date";
-
+type Attr = "programId" | "trackedEntityId" | "orgUnitId";
 type Row = Record<Attr, string>;
 
 const headers: Record<Attr, { title: string }> = {
-    id: { title: "Event ID" },
-    date: { title: "Event Date" },
-    created: { title: "Created" },
-    status: { title: "Status" },
-    program: { title: "Program" },
-    programStage: { title: "Program Stage" },
-    orgUnit: { title: "Org Unit" },
-    teiId: { title: "TEI ID" },
-    dataValues: { title: "Data values" },
+    programId: { title: "Program ID" },
+    trackedEntityId: { title: "Tracked Entity ID" },
+    orgUnitId: { title: "Org Unit ID" },
 };
