c51efe3403101729f262b59ae240a4657978a92a
p3rcypj <pablo@percypj.com>
2023-05-03 15:03:58 +0000

    Report errors correctly

--- src/domain/repositories/TrackerRepository.ts
+++ src/domain/repositories/TrackerRepository.ts
@@ -1,10 +1,11 @@
-import { BundleReport } from "data/TrackerD2Repository";
+import { BundleReport, ValidationReport } from "data/TrackerD2Repository";
 import { Async } from "domain/entities/Async";
 import { Enrollment, Event, TrackedEntity } from "domain/entities/TrackedEntity";
+import { Maybe } from "utils/ts-utils";
 
 export interface TrackerRepository {
     get(options: GetOptions): Async<TrackedEntity[]>;
-    save(payload: ClosurePayload): Async<BundleReport>;
+    save(payload: ClosurePayload): Async<BundleReport | Maybe<ValidationReport>>;
 }
 
 export interface GetOptions {
